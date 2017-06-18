82efe70fb7ed64133379d4ece28f26abd55ab902
姬世霆_Jacky <jishiting@users.noreply.github.com>
2017-06-18 10:43:53 +0300

    first commit

---
+++ src/main/java/graph/Graph.java
@@ -0,0 +1,11 @@
+package graph;
+
+/**
+ * Created by 111 on 6/18/2017.
+ */
+public class Graph {
+    public void init(){
+        System.out.print("hello graph");
+    }
+
+}
---
+++ src/test/java/graph/GraphTest.groovy
@@ -0,0 +1,10 @@
+package graph
+
+import spock.lang.Specification
+
+/**
+ * Created by 111 on 6/18/2017.
+ */
+class GraphTest extends Specification {
+
+}
